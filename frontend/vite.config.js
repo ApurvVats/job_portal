8921f53d10ee4a608a363e9df086d4fa444172cc
Himanshu Kumar <himanshukr0008@gmail.com>
2025-05-17 15:57:11 +0530

    u

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,10 @@
 import path from "path"
-import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // Ensures routing works when deployed
   plugins: [react()],
   resolve: {
     alias: {
