cb00fde764163014b20f8c1a9822e41d28522578
Himanshu Kumar <himanshukr0008@gmail.com>
2025-05-17 12:50:35 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,7 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions));
+ 
 
 const PORT = process.env.PORT || 3000;
 
@@ -31,7 +32,7 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
-
+ 
 
 
 app.listen(PORT,()=>{
