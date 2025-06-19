cb4a8f7884be93b760ad67ad82d2df34e77ec487
Apurvvats <apurvvts@gmail.com>
2025-06-19 21:16:43 +0530

    new commit

--- backend/index.js
+++ backend/index.js
@@ -32,11 +32,6 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
- 
-app.get("/", (req, res) => {
-    res.send({ activeStatus: true, error: false });
-});
-
 
 app.listen(PORT,()=>{
     connectDB();
